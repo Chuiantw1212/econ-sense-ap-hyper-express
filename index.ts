69ca436801855d9621e19111ea5cf26e155ec89b
Chuian <chuiantw1212@gmail.com>
2024-05-17 13:30:03 +0800

    add ignore

--- index.ts
+++ index.ts
@@ -13,7 +13,6 @@ import jcicModel from './models/jcic'
 import locationModel from './models/location'
 import userModel from './models/user';
 import ndcModel from './models/ndc';
-import sitcaModel from './models/sitca';
 // controllers
 import rootController from './controllers/root'
 import bankController from './controllers/bank'
@@ -43,7 +42,6 @@ import userController from './controllers/user'
         locationModel
     })
     ndcModel.initialize(firestore)
-    sitcaModel.initialize()
     // controllers
     const corsConfig: CorsOptions = {
         origin: process.env.ORIGIN || 'http://localhost:5173',
