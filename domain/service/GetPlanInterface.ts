ad83307795753205e02715078bd909bc5009230b
Chuian <chuiantw1212@gmail.com>
2024-09-14 23:58:51 +0800

    wip

--- domain/service/GetPlanInterface.ts
+++ domain/service/GetPlanInterface.ts
@@ -1,4 +1,7 @@
 import type GetPlanUseCase from '../../useCases/GetPlanUseCase'
 class GetPlanInterfaceService implements GetPlanUseCase {
-    
+    getPlanInterface() {
+        
+    }
 }
+export default GetPlanInterfaceService
