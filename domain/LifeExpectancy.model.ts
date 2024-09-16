beada6049e3808a66541f406f1a0712cb2eefe8b
Chuian <chuiantw1212@gmail.com>
2024-09-16 20:15:01 +0800

    wip

--- domain/LifeExpectancy.model.ts
+++ domain/LifeExpectancy.model.ts
@@ -25,34 +25,4 @@ export default class LifeExpectancyModel {
             return 0
         }
     }
-    // async uploadFiles() {
-    //     const dataset: INdcLifeExpectancyRawItem[] = import('./8bef5427a12789acdf8c0411131d9221_export.json')
-    //     const hasGenderItem = dataset.filter(item => {
-    //         return ['男性', '女性'].includes(item['性別'])
-    //     })
-
-    //     const countData: DocumentData = await this.collectionLifeExpectancy.count().get()
-    //     const count: number = countData.data().count
-    //     const items: ILifeExpectancyItem[] = hasGenderItem.slice(count, hasGenderItem.length).map(item => {
-    //         const genderMap: { [key: string]: string } = {
-    //             "男性": "M",
-    //             "女性": "F"
-    //         }
-    //         const gender = genderMap[item['性別']]
-    //         const ageItem = item['年齡'].replace('歲', '')
-    //         return {
-    //             ceYear: Number(item['西元年']),
-    //             rocYear: Number(item['民國年']),
-    //             gender: gender,
-    //             age: Number(ageItem),
-    //             lifeExpectancy: Number(item['預期壽命']),
-    //         }
-    //     })
-    //     let index = 0
-    //     setInterval(async () => {
-    //         const item = items[index++]
-    //         await this.collectionLifeExpectancy.add(item)
-    //         console.log(`total ${items.length}, ${index} added.`)
-    //     }, 100)
-    // }
 }
