67be32d55dd1a549051307c07ae168f48927bf06
Chuian <chuiantw1212@gmail.com>
2024-09-16 13:37:44 +0800

    add api

--- adapters/blog.in/calculate.ctrl.ts
+++ adapters/blog.in/calculate.ctrl.ts
@@ -4,6 +4,27 @@ import jcicModel from '../../domain/jcic.model'
 import { ILocals } from '../../entities/app'
 
 const router = new HyperExpress.Router()
+
+router.post('/finance/lifeExpectancy', async function (req, res) {
+    try {
+        const locals = req.app.locals as ILocals
+        const body = await req.json()
+        const lifeExpectancy = await locals.GetLifeExpectancyService.getLifeExpectancy(body)
+        res.json(lifeExpectancy)
+    } catch (error: any) {
+        res.send(error.message || error)
+    }
+})
+router.post('/finance/unitPrice', async function (req, res) {
+    try {
+        const body = await req.json()
+        const result = await jcicModel.calculateUnitPrice(body)
+        res.json(result)
+    } catch (error: any) {
+        res.send(error.message || error)
+    }
+})
+// deprecated
 router.post('/calculate/lifeExpectancy', async function (req, res) {
     try {
         const locals = req.app.locals as ILocals
@@ -14,6 +35,7 @@ router.post('/calculate/lifeExpectancy', async function (req, res) {
         res.send(error.message || error)
     }
 })
+// deprecated
 router.post('/calculate/unitPrice', async function (req, res) {
     try {
         const body = await req.json()
