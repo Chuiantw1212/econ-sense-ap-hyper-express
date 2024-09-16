57eec7a5ecf731669c7a67c709b70a310c9a1f93
Chuian <chuiantw1212@gmail.com>
2024-09-16 17:41:10 +0800

    refactor completed

--- adapters/blog.in/select.ctrl.ts
+++ adapters/blog.in/select.ctrl.ts
@@ -1,14 +1,14 @@
 import HyperExpress from 'hyper-express'
-// import locationModel from '../../domain/Location.model'
+import { ILocals } from '../../entities/app'
 const router = new HyperExpress.Router()
 /**
  * Deprecated, 合併到meta
  */
 router.get('/select', async function (req, res) {
     try {
-        const locals = req.app.locals as any
-        const countiesAndTownMap = await locals.GetTaiwanLocationService.getTaiwanLocations()
-        const selectOptionsMap = await locals.GetOptionsService.getOptionsMap()
+        const { GetTaiwanLocationService, GetOptionsService } = req.app.locals as ILocals
+        const countiesAndTownMap = await GetTaiwanLocationService.getTaiwanLocations()
+        const selectOptionsMap = await GetOptionsService.getOptionsMap()
         const result = {
             ...countiesAndTownMap,
             ...selectOptionsMap,
